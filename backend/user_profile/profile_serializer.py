86a4486574842f9d476eac36cf17d95fcd0730fe
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-02-14 15:45:27 +0100

    Fetch user profile and Update user profile completed

--- backend/user_profile/profile_serializer.py
+++ backend/user_profile/profile_serializer.py
@@ -5,8 +5,10 @@ from .models import UserProfileModel
 class ProfileSerializer(serializers.ModelSerializer):
     """Update or get profile serializer"""
     profile_img = serializers.ImageField(required=False)
+    email = serializers.EmailField(source="user.email", read_only=True)
+    user_id = serializers.CharField(source="user.id", read_only=True)
 
     class Meta:
         model = UserProfileModel
-        fields = "__all__"
-        read_only_fields = ['user']
+        fields = ["id", "first_name", "last_name", "display_name", "profile_img",
+                  "country", "state", "city", "address", "email", "user_id"]
--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -1,14 +1,15 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .profile_serializer import ProfileSerializer
+from .models import UserProfileModel
 # Create your views here.
 
 
 class UserProfileView(APIView):
     """Create, Update, get user profile"""
-    print("hello")
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
@@ -20,5 +21,30 @@ class UserProfileView(APIView):
             serializer.is_valid(raise_exception=True)
             serializer.save(user=request.user)
             return Response({"message": "Profile saved", "data": serializer.data})
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request):
+        """Update user profile"""
+        profile = get_object_or_404(
+            UserProfileModel, user=request.user)
+        serializer = ProfileSerializer(
+            profile, data=request.data, partial=True)
+        try:
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({"code": 1, "data": serializer.data}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        """Fetch user profile"""
+        try:
+            profile = get_object_or_404(
+                UserProfileModel, user=request.user)
+            serializer = ProfileSerializer(profile)
+            return Response({"code": 1, "data": serializer.data}, status=status.HTTP_200_OK)
+
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
