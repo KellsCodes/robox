2095c921e18dfe93e31f611ea68df49597ae5694
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-03-04 13:13:07 +0100

    Improve Trades Model to accomodate AI analytics

--- backend/trades/models.py
+++ backend/trades/models.py
@@ -10,36 +10,30 @@ class Trade(models.Model):
         UserAccount, on_delete=models.CASCADE, related_name="trades")
     pair = models.CharField(max_length=100, blank=False,
                             null=False)
-    session = models.CharField(max_length=100, blank=True, default="")
-    general_market_condition = models.CharField(
+    session = models.CharField(max_length=100, blank=True, null=True)
+    entry_type = models.CharField(max_length=100, blank=False, null=False)
+    order_type = models.CharField(
         max_length=100, blank=False, null=False)
-    intermediate_market_condition = models.CharField(
-        max_length=100, blank=False, null=False)
-    entry_market_condition = models.CharField(
-        max_length=100, blank=False, null=False)
-    entry_strategy = models.CharField(
-        max_length=255, blank=False, null=False)
-    trade_entry_description = models.CharField(
-        max_length=255, blank=True, null=True)
     entry_price = models.DecimalField(
         max_digits=10, decimal_places=5, blank=False, null=False)
     stoploss_price = models.DecimalField(
         max_digits=10, decimal_places=5, blank=False, null=False)
-    entry_type = models.CharField(max_length=100, blank=False, null=False)
-    order_type = models.CharField(
-        max_length=100, blank=False, null=False, default="market execution")
-    status = models.CharField(max_length=100, blank=False, null=False)
-    outcome = models.CharField(max_length=100, blank=False, null=False)
-    manual_exit_reason = models.CharField(
+    risk_reward_ratio = models.FloatField(null=True, blank=True)
+    trade_duration = models.PositiveIntegerField(
+        blank=True, null=True)  # Trade duration in minutes
+    status = models.CharField(max_length=50, choices=[
+                              ("Open", "Open"), ("Closed", "Closed")])
+    outcome = models.CharField(max_length=50, choices=[(
+        "Profit", "Profit"), ("Loss", "Loss"), ("Breakeven", "Breakeven")])
+    execution_time = models.DateTimeField(blank=True, null=True)
+    trade_description = models.CharField(
         max_length=255, blank=True, null=True)
     image_url = models.URLField(max_length=255, blank=True, null=True)
-    time_executed = models.DateTimeField(blank=True, null=True)
-    trade_duration = models.PositiveSmallIntegerField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Trade {self.id} - {self.pair} ({self.entry_strategy})"
+        return f"Trade {self.id} - {self.pair} {self.entry_type} ({self.status})"
 
 
 class ProfitTarget(models.Model):
