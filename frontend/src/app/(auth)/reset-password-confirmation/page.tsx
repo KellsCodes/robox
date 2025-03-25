924b86703199391233dd3dc016f5e84bb9180b7e
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-03-25 12:17:31 +0100

    Frontend project setup

---
+++ frontend/src/app/(auth)/reset-password-confirmation/page.tsx
@@ -0,0 +1,7 @@
+export default function PasswordResetConfirmation() {
+    return (
+        <div>
+            <h1>Reset password confirmation page</h1>
+        </div>
+    )
+}
---
+++ frontend/src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,7 @@
+export default function PasswordReset() {
+    return (
+        <div>
+            <h1>Reset password page</h1>
+        </div>
+    )
+}
